43e8d738b92f6a8f51b3ba34773d153d1831e301
alsha <alshabehera7@gmail.com>
2023-02-19 09:28:42 +0530

    leap yr

---
+++ practice.py
@@ -0,0 +1,41 @@
+#module
+"""import fibonacci
+print(fibonacci.c)"""
+
+"""x=int(input("Enter:"))
+z=x//10
+print(z)"""
+ 
+#swapping
+"""x,y=map(int,input("Enter:").split())
+z=x
+x=y
+y=z
+print("x:",x," y:",y)"""
+
+#divisible by 5
+"""x=int(input("Enter:"))
+if x%5==0:
+    print("Yes")
+else:    
+    print("NO") """
+
+"""x=int(input("Enter:"))
+x=str(x)
+if len(x)==3:
+    print("yes")
+else:
+    print("NO")  """
+
+ #leap year
+x=int(input("Enter a year:"))
+if x%400==0 and x%100==0:
+    print("YES")
+elif x%4==0 and x%100==0:
+    print("YES") 
+else:
+    print("NO")    
+
+  
+
+                 
